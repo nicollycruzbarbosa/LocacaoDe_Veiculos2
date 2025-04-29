3de4134088580e803297b6b3417ca52c8b089a9a
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2025-04-29 09:31:12 -0300

    Update Carro.php

---
+++ models/Carro.php
@@ -0,0 +1,29 @@
+<?php
+namespace Models;
+use Interfaces\Locavel;
+
+// Classe que representa um carro
+
+class Carro extends Veiculo implements Locavel {
+
+    public function calcularAluguel(int $dias):float {
+        return $dias * DIARIA_CARRO;
+    }
+
+    public function alugar(): string {
+        if ($this->disponivel){
+            $this->disponivel = false;
+            return "Carro '{$this->modelo}' alugado com sucesso!";
+        }
+        return "Carro '{$this->modelo}' não está disponível no momento";
+    }
+
+    public function devolver(): string {
+        if (!$this->disponivel){
+            $this->disponivel = true;
+            return "Carro '{$this->modelo}' devolvido com sucesso!";
+        }
+        return "Carro '{$this->modelo}' está disponível no momento";
+}
+
+}
